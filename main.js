42099ee77e1c052d669151891cfdc8e7ef9f743e
Daniel <dwalker.prsvr@gmail.com>
2023-09-20 13:37:31 -0400

    finished

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("item-16").classList.add("item")
+// document.getElementById("item-16").className = "item"
+let heading = document.querySelector('h1')
+let paragraph = document.querySelector('p')
+let items = document.querySelectorAll('.item')
+let image = document.querySelectorAll('img')
+
+paragraph.style.color = 'lightblue'
+heading.style.fontSize = '48px'
+paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+items[12].style.opacity = .5
+items[2].innerHTML = 'I say, "Hi!"'
+document.getElementById("pasta").src="http://www.tioxic.com/wp-content/uploads/trex_4.jpg"
+image[0].style.height = '300px'
+image[1].style.height = '300px'
+items[15].innerHTML = 'Won\'t get fooled again.'
